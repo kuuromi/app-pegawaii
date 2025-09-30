b3a2e20cff91f31ea14b43a65c222632c8c8d49d
kuuromi <rosaliaarlusi@gmail.com>
2025-09-30 17:50:52 +0700

    CRUD tabel employee

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
+// route untuk default
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route resource untuk employees
+Route::resource('employees',EmployeeController::class);
