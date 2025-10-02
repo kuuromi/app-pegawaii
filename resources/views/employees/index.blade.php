b615edaaed3a378ae63b9f2b3df05b826ec2efd4
kuuromi <rosaliaarlusi@gmail.com>
2025-10-02 13:47:40 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   
-</head>
-<body>
-    <div class="container mt-5">
-        <h1 class="mb-4">Daftar Pegawai</h1>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-4">Daftar Pegawai</h1>
     <table border="1" cellpadding="5" cellspacing="0">
         <thead>
             <tr>
@@ -20,7 +17,7 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($employees as $employee)
+            @foreach($employees as $employee)
             <tr>
                 <td>{{ $employee->nama_lengkap }}</td>
                 <td>{{ $employee->email }}</td>
@@ -30,17 +27,17 @@
                 <td>{{ $employee->tanggal_masuk }}</td>
                 <td>{{ $employee->status }}</td>
                 <td>
-                    <a href="{{ route('employees.show', $employee->id)}}">Detail</a> |
-                    <a href="{{ route('employees.edit', $employee->id)}}">Edit</a> |
+                    <a href="{{ route('employees.show', $employee->id) }}">Detail</a> |
+                    <a href="{{ route('employees.edit', $employee->id) }}">Edit</a> |
                     <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Detete </button>
+                        <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Delete</button>
                     </form>
                 </td>
-            </tr>   
+            </tr>
             @endforeach
         </tbody>
     </table>
-</body>
-</html>
+</div>
+@endsection
